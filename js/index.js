c07a4d3c8713a572b7375f3d179931bcf9af516c
ag47 <ag47morning@gmail.com>
2020-10-26 03:13:52 +0900

    scroll event

--- js/index.js
+++ js/index.js
@@ -1,12 +1,45 @@
-$(function(){
-    ////// scroll {{ //////
+$(document).ready(function(){
+    ////// scroll event {{ //////
     var i = 0;
-    var a = $("#news").scrollTop();
-    $(window).on("mousewheel DOMMousewheel", function(e){
-        var dt = e.originalEvent.wheelDelta;
-        console.log(dt);
-        console.log(a);
+    var a;
+    var topArray = new Array();
+    for (var j = 0; j<$("#tnb li").length; j++) {
+        a = $("#container section").eq(j).offset().top;
+        a = Number(a);
+        topArray.push(a);
+    }
+    console.log(topArray);
+    $(window).on("mousewheel DOMMousewheel scroll", function(){
+        var winH = $(this).scrollTop();
+        console.log("현재 "+ winH + typeof winH);
+        console.log(window.pageYOffset);
+        if(winH < topArray[1]) i=0;
+        else if (winH < (topArray[2])) i=1;
+        else if (winH < (topArray[3])) i=2;
+        else if (winH < (topArray[4])) i=3;
+        else if (winH >= (topArray[4])) i=4;
+        console.log(i);
+        $("#tnb li").removeClass("active");
+        $("#tnb li").eq(i).addClass("active");
+    })
+    
+    var ani = false;
+    $("#tnb li").on("click", function(e){
+        if(ani) return;
+        ani = true;
+        e.preventDefault();
+        i = $(this).index();
+        $("#tnb li").removeClass("active");
+        $("#tnb li").eq(i).addClass("active");
+        $("html, body").stop().animate({scrollTop:topArray[i]},500,function(){ani=false;});
     })
 
-    ////// }} scroll //////
+    ////// }} scroll event //////
+})
+
+$(function(){
+    //// 새로고침 시 scrollTop : 0
+    $(window).on("beforeunload", function(){
+        $("html, body").stop().animate({scrollTop:0},0);
+    })
 })
