dbb0e8f38201ac14e9eb9a1d55b6b70c795ac55c
ag47 <ag47morning@gmail.com>
2020-10-27 14:45:31 +0900

    section clear

--- js/index.js
+++ js/index.js
@@ -19,11 +19,13 @@ $(document).ready(function(){
         else {
             $("#hd").css("background","");
         }
+
         if(winH < topArray[1]-300) i=0;
         else if (winH < (topArray[2]-300)) i=1;
         else if (winH < (topArray[3]-300)) i=2;
         else if (winH < (topArray[4]-300)) i=3;
-        else if (winH >= (topArray[4]-300)) i=4;
+        else if (winH < (topArray[5]-300)) i=4;
+        else if (winH >= (topArray[5]-300)) i=5;
         // console.log(i);
         $("#tnb li").removeClass("active");
         $("#tnb li").eq(i).addClass("active");
@@ -49,3 +51,26 @@ $(document).ready(function(){
 //         $("html, body").stop().animate({scrollTop:0},0);
 //     })
 // })
+
+$(function(){
+    //// view more click event
+    var card_desc = $("#service .card.desc");
+    var descH = card_desc.eq(0).innerHeight();
+    var serviceH = descH * (card_desc.length+1);
+    $("#service .con_more").on("click",function(e){
+        e.preventDefault();
+        $("#service .con_more").css("display","none");
+        $("#service .card_wr").css("max-height",serviceH+"px");
+        $("html, body").stop().animate({scrollTop:$("#service").offset().top + (descH*3)},300);
+    })
+
+    var thumb = $("#news .thumb");
+    var thumbH = $("#news .thumb").eq(0).outerHeight(true);
+    var newsH = thumbH * Math.ceil(thumb.length/4);
+    $("#news .con_more").on("click",function(e){
+        e.preventDefault();
+        $("#news .con_more").css("display","none");
+        $("#news .thumb_wr").css("max-height",newsH+"px");
+        $("html, body").stop().animate({scrollTop:$("#news").offset().top + thumbH},300);
+    })
+})
